285afa701c23d74919d4e5ae9ae69603d71a2d4f
Nicollas-Trovao <137838083+Nicollas-Trovao@users.noreply.github.com>
2024-04-01 21:07:43 -0300

    Delete Projeto 3 directory

--- Projeto 3/src/main/Principal/DAO/IClienteDAO.java
+++
@@ -1,9 +0,0 @@
-package main.Principal.DAO;
-
-import main.Principal.DAO.Generic.IGenericDAO;
-import main.Principal.domain.Cliente;
-
-public interface IClienteDAO extends IGenericDAO<Cliente, Long> {
-
-
-}
--- Projeto 3/src/main/Principal/services/IProdutoService.java
+++
@@ -1,8 +0,0 @@
-package main.Principal.services;
-
-import main.Principal.domain.Produto;
-import main.Principal.services.generic.IGenericService;
-
-public interface IProdutoService extends IGenericService<Produto, String> {
-
-}

20fe62a5fb5c54e20de920ae9c583d11a995c8b2
Nicollas-Trovao <137838083+Nicollas-Trovao@users.noreply.github.com>
2024-04-01 21:12:01 -0300

    Delete Projeto 3 directory

--- Projeto 3/src/main/Principal/DAO/IProdutoDAO.java
+++
@@ -1,8 +0,0 @@
-package main.Principal.DAO;
-
-import main.Principal.DAO.Generic.IGenericDAO;
-import main.Principal.domain.Produto;
-
-public interface IProdutoDAO extends IGenericDAO<Produto, String>{
-
-}
